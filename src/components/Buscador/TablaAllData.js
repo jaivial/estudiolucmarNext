3e2013d9a02fb711bb7c183daa5aedd412024b6a
jaivial <114494844+jaivial@users.noreply.github.com>
2024-08-21 10:56:48 +0200

    updated mobileMenuBar.js with accordion for moreMobileMenu, updated icons from svg long code to react-icons and iconify-icons

--- src/components/Buscador/TablaAllData.js
+++ src/components/Buscador/TablaAllData.js
@@ -13,7 +13,8 @@ import SmallLoadingScreen from '../LoadingScreen/SmallLoadingScreen.js';
 import Select from 'react-select';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import { set } from 'date-fns';
+import { Icon } from '@iconify/react';
+
 
 const Table = ({ parentsEdificioProps }) => {
     const [loading, setLoading] = useState(true);
@@ -1041,20 +1042,10 @@ const Table = ({ parentsEdificioProps }) => {
                             </div>
                             <div className="flex flex-row justify-end items-center gap-3 w-[40%]">
                                 {child.noticiastate === true && (
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="2.1em" height="2.1em" viewBox="0 0 24 24">
-                                        <path
-                                            fill="currentColor"
-                                            d="M10 7h4V5.615q0-.269-.173-.442T13.385 5h-2.77q-.269 0-.442.173T10 5.615zm8 15q-1.671 0-2.835-1.164Q14 19.67 14 18t1.165-2.835T18 14t2.836-1.165T22 18t-1.164 2.836T18 22M4.615 20q-.69 0-1.153-.462T3 18.384V8.616q0-.691.463-1.153T4.615 7H9V5.615q0-.69.463-1.153T10.616 4h2.769q.69 0 1.153.462T15 5.615V7h4.385q.69 0 1.152.463T21 8.616v4.198q-.683-.414-1.448-.614T18 12q-2.496 0-4.248 1.752T12 18q0 .506.086 1.009t.262.991zM18 20.423q.2 0 .33-.13t.132-.331t-.131-.331t-.331-.13t-.33.13t-.132.332t.131.33t.331.131m-.385-1.846h.77v-3h-.77z"
-                                        />
-                                    </svg>
+                                    <Icon icon="material-symbols:work-alert" />
                                 )}
                                 {child.encargostate === true && (
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 20 20">
-                                        <path
-                                            fill="currentColor"
-                                            d="M2 3a1 1 0 0 1 2 0h13a1 1 0 1 1 0 2H4v12.5a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5v7a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 5 13.5zm3 7a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-2.55a1 1 0 0 0-.336-.748L11.332 8.13a.5.5 0 0 0-.664 0L8.336 10.2a1 1 0 0 0-.336.75z"
-                                        />
-                                    </svg>
+                                    <Icon icon="fluent:real-estate-24-filled" />
                                 )}
                                 <div onClick={() => handleItemClick(child.id)} className="cursor-pointer w-[20%] mr-4">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="2.1em" height="2.1em" viewBox="0 0 16 16" className="text-cyan-800 bg-white rounded-full hover:w-[2.5em] hover:h-[2.5em] hover:shadow-lg hover:text-cyan-600">
