615715ff5ec3709fa02bf29c36e618f62ead88e5
jaivial <114494844+jaivial@users.noreply.github.com>
2024-10-28 13:55:17 +0100

    Cambiar botones de mobilenavbar a botones defitnitivos

--- src/components/MenuBars/MobileMenuBar.js
+++ src/components/MenuBars/MobileMenuBar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { FaHome, FaSearch, FaMapMarkedAlt, FaTasks, FaNewspaper, FaCog, FaEllipsisV, FaSignOutAlt } from 'react-icons/fa';
+import { AiOutlineHome, AiOutlineSearch, AiOutlineSetting, AiOutlineUser } from 'react-icons/ai';
 import { Icon } from '@iconify/react';
 import Cookies from 'js-cookie';
 import axios from 'axios';
@@ -36,10 +37,8 @@ const MobileMenuBar = () => {
             setCurrentPage('Home');
         } else if (currentURL.includes('settings')) {
             setCurrentPage('Settings');
-        } else if (currentURL.includes('encargos')) {
-            setCurrentPage('Encargos');
-        } else if (currentURL.includes('noticias')) {
-            setCurrentPage('Noticias');
+        } else if (currentURL.includes('clientes')) {
+            setCurrentPage('Clientes');
         } else if (currentURL.includes('mapa')) {
             setCurrentPage('Mapa');
         } else if (currentURL.includes('mizona')) {
@@ -104,13 +103,13 @@ const MobileMenuBar = () => {
             <nav className="h-10 w-full flex flex-row justify-between border-t border-slate-300 shadow-2xl fixed bottom-0 left-0 z-[9999] overflow-visible bg-slate-50">
                 <ul className="w-full h-full flex flex-row justify-around items-end transition-all pb-3.5 overflow-y-visible shadow-xl">
                     <Link href="/home" className={`${isActive('Home')} px-3.5 rounded-3xl shadow-lg flex items-center justify-center`}>
-                        <FaHome size="1.8em" />
+                        <AiOutlineHome className='text-3xl' />
                     </Link>
                     <Link href="/mizona" className={`${isActive('Mizona')} px-4 rounded-3xl shadow-lg flex items-center justify-center`}>
-                        <Icon icon="mdi:map-marker-radius" className='text-3xl' />
+                        <Icon icon="gis:poi-map" className='text-3xl' />
                     </Link>
                     <Link href="/mapa" className={`${isActive('Mapa')} px-4 rounded-3xl shadow-lg flex items-center justify-center`}>
-                        <Icon icon="gis:poi-map" className='text-3xl' />
+                        <Icon icon="solar:map-linear" className='text-3xl' />
                     </Link>
                     <div
                         id="mobile-itemMoreMenuIcon"
@@ -127,12 +126,6 @@ const MobileMenuBar = () => {
                                 }`}
                         >
                             <ul className="w-full flex flex-col justify-around items-center gap-8">
-                                <Link href="/encargos" onClick={handleLinkClick} className={`${isActive('Encargos')} w-full flex justify-center px-3.5 py-2 rounded-3xl shadow-lg`}>
-                                    <Icon icon="fluent:real-estate-24-filled" className='text-3xl' />
-                                </Link>
-                                <Link href="/noticias" onClick={handleLinkClick} className={`${isActive('Noticias')} w-full flex justify-center px-3.5 py-2 rounded-3xl shadow-lg`}>
-                                    <Icon icon="material-symbols:work-alert" className='text-3xl' />
-                                </Link>
                                 <Link href="/settings" onClick={handleLinkClick} className={`${isActive('Settings')} w-full flex justify-center px-3.5 py-2 rounded-3xl shadow-lg`}>
                                     <FaCog size="1.8em" />
                                 </Link>
